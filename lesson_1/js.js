c235f64114f5aa44860b18eb354cb66f08035d01
DimaKashyrin <kashyrindima@gmail.com>
2021-07-12 21:06:02 +0300

    added first part

---
+++ lesson_1/js.js
@@ -0,0 +1,57 @@
+//part - 1
+
+let greeting = 'hello';
+let companyName = 'owu';
+let  domGlobal = 'com';
+let domUa = 'ua';
+let testNumber1 = 1;
+let testNumber2 = 10;
+let testNumber3 = -999;
+let testNumber4 = 123;
+let testNumber5 = 3.14;
+let testNumber6 = 2.7;
+let testNumber7 = 16;
+let bool1 = true;
+let bool0 = false;
+
+console.log(greeting);
+console.log(companyName);
+console.log(domGlobal);
+console.log(domUa);
+console.log(testNumber1);
+console.log(testNumber2);
+console.log(testNumber3);
+console.log(testNumber4);
+console.log(testNumber5);
+console.log(testNumber6);
+console.log(testNumber7);
+console.log(bool1);
+console.log(bool0);
+
+alert(greeting);
+alert(companyName);
+alert(domGlobal);
+alert(domUa);
+alert(testNumber1);
+alert(testNumber2);
+alert(testNumber3);
+alert(testNumber4);
+alert(testNumber5);
+alert(testNumber6);
+alert(testNumber7);
+alert(bool1);
+alert(bool0);
+
+document.write(`<div> ${greeting} </div>`);
+document.write(`<div> ${companyName} </div>`);
+document.write(`<div> ${domGlobal} </div>`);
+document.write(`<div> ${domUa} </div>`);
+document.write(`<div> ${testNumber1} </div>`);
+document.write(`<div> ${testNumber2} </div>`);
+document.write(`<div> ${testNumber3} </div>`);
+document.write(`<div> ${testNumber4} </div>`);
+document.write(`<div> ${testNumber5} </div>`);
+document.write(`<div> ${testNumber6} </div>`);
+document.write(`<div> ${testNumber7} </div>`);
+document.write(`<div> ${bool1} </div>`);
+document.write(`<div> ${bool0} </div>`);
