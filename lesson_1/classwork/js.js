5ee3e3c136feaa838a272ebf09bf23b271e0a53e
DimaKashyrin <kashyrindima@gmail.com>
2021-07-13 15:15:09 +0300

    added second part

--- lesson_1/classwork/js.js
+++ lesson_1/classwork/js.js
@@ -6,3 +6,15 @@ let flag = true;
 let txt = 'true';
 
 console.log(typeof str, typeof num, typeof flag, typeof txt);
+console.log('++++++++++++++++++++++++++++++++++++');
+
+// part 2
+let a1 = 15 * 2 + 4;        //34
+let a2 = (20 - 18) * 6;     //12
+let a3 = (99 / 3) * 2;      //66
+let a4 = (90 + 90) / 2;     //90
+let a5 = 30 * 3 - 3;        //87
+
+console.log (a1,a2,a3,a4,a5)
+console.log('++++++++++++++++++++++++++++++++++++');
+
