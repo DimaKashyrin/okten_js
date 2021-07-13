ca4a585bb218dd11d6af45998f50db23c9218a2b
DimaKashyrin <kashyrindima@gmail.com>
2021-07-13 18:05:30 +0300

    added 9 part

--- lesson_1/classwork/js.js
+++ lesson_1/classwork/js.js
@@ -59,3 +59,12 @@ let hobby = 'learning';
 
 alert(pib + '\n' + age + '\n' + hobby)
 
+//part 9
+let str1 = 'Кто ';
+let str2 = 'ты ';
+let str3 = 'такой?'
+let concatenation = str1 + str2 + str3;
+
+document.write(concatenation)
+
+
