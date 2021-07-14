c9eb7bb729e0c8c874585961af46487601bf632d
DimaKashyrin <kashyrindima@gmail.com>
2021-07-14 15:25:52 +0300

    added solution additional_1.1

---
+++ lesson_2/additional_task/solution/javaScript/js_1.js
@@ -0,0 +1,12 @@
+// task - 1  ===========================================
+
+let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+
+document.write('<h3>task - 1</h3>')
+document.write('<ul>')
+for (let listOfItem of listOfItems) {
+    document.write(`<li>${listOfItem}</li>`)
+}
+document.write('</ul>')
+document.write('<hr>')
+
