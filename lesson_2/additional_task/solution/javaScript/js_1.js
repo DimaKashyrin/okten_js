767cf7d545c6b493846d13e92271ce99ab8c783d
DimaKashyrin <kashyrindima@gmail.com>
2021-07-14 22:41:38 +0300

    added solution additional_1.3.1

--- lesson_2/additional_task/solution/javaScript/js_1.js
+++ lesson_2/additional_task/solution/javaScript/js_1.js
@@ -95,3 +95,44 @@ document.write('<h3>task - 2.3</h3>')
         }
 
     document.write('</div>')
+
+document.write('<h3>task - 3.1</h3>')
+
+let products = [
+    {
+        title: 'milk',
+        price: 22,
+        image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+    },
+    {
+        title: 'juice',
+        price: 27,
+        image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+    },
+    {
+        title: 'tomato',
+        price: 47,
+        image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+    },
+    {
+        title: 'tea',
+        price: 15,
+        image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+    },
+];
+document.write('<div class="wrap-product-cards">')
+
+    for (let product of products) {
+
+        document.write('<div class="product-card">')
+            document.write(`
+                            <h3 class="product-title">
+                                <span class="product-name">${product.title}</span>
+                                <span class="product-price">Price - ${ product.price}</span>
+                            </h3>
+                        `)
+            document.write(`<img src="${product.image}" alt="image" class="product-image">`)
+        document.write('</div>')
+    }
+
+document.write('</div>')
