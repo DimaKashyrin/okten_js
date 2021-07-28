cd433c206ef62e580874d9ae0d09b78e5867673d
DimaKashyrin <kashyrindima@gmail.com>
2021-07-28 18:48:48 +0300

    added solution classwork ( lesson - 6  )

--- lesson_6/classwork/solution/js.js
+++ lesson_6/classwork/solution/js.js
@@ -7,42 +7,52 @@ fetch('https://jsonplaceholder.typicode.com/posts')
   .then(value => value.json())
   .then(value => {
     
-    let wrap = document.getElementsByClassName('wrap')[0];
-    let id = document.createElement('div');
-    let title = document.createElement('h2');
-    let text = document.createElement('p');
-    let userId = document.createElement('span');
-    let button = document.createElement('button');
-    let footerPost = document.createElement('div');
-    let popUp = document.createElement('div');
-    
-    id.classList.add('id-class');
-    title.classList.add('title');
-    text.classList.add('context');
-    userId.classList.add('user-post');
-    button.classList.add('btn');
-    footerPost.classList.add('footer');
-    popUp.classList.add('pop-up')
-    
-    
-    
-      for (let post of value) {
-        let postCard = document.createElement('div');
-        postCard.classList.add('post-card')
-        id.innerHTML = `post # ${post.id}`;
-        title.innerHTML = `${post.title}`;
-        text.innerHTML = `${post.body}`;
-        userId.innerHTML = `user # ${post.userId}`;
-        button.innerHTML = 'more...';
-        button.onclick = function () {
-          console.log('test')
+    for (let post of value) {
+      
+      let wrap = document.getElementsByClassName('wrap')[0];
+      let id = document.createElement('div');
+      let title = document.createElement('h2');
+      let text = document.createElement('p');
+      let userId = document.createElement('span');
+      let button = document.createElement('button');
+      let footerPost = document.createElement('div');
+      let popUp = document.createElement('div');
+      let postCard = document.createElement('div');
+  
+      id.classList.add('id-class');
+      title.classList.add('title');
+      text.classList.add('context');
+      userId.classList.add('user-post');
+      button.classList.add('btn');
+      footerPost.classList.add('footer');
+      popUp.classList.add('pop-up');
+      postCard.classList.add('post-card')
+      
+      id.innerHTML = `post # ${post.id}`;
+      title.innerHTML = `${post.title}`;
+      text.innerHTML = `${post.body}`;
+      userId.innerHTML = `user # ${post.userId}`;
+      button.innerHTML = 'more...';
+      
+      button.onclick = function (){
+        popUp.innerHTML = '';
+        for (let searchPost of value) {
+          if(searchPost.userId === post.userId){
+            popUp.innerHTML += `
+              <div>${searchPost.id}</div>
+              <h3>${searchPost.title}</h3>
+              <p>${searchPost.body}</p>
+              <hr>
+            `;
+          }
         }
-        footerPost.append(userId,button)
-        postCard.append(id, title, text,footerPost);
-        
-        wrap.appendChild(postCard.cloneNode(true));
-       
+        postCard.append(popUp);
+        popUp.classList.toggle('pop-up');
       }
-    
-    
+      
+      footerPost.append(userId,button);
+      postCard.append(id, title, text,footerPost);
+      wrap.appendChild(postCard);
+      
+    }
   })
